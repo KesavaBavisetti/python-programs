cfa6068784dd549821673c7758477fffa74a0b5b
Kesava Bavisetti <kesavabavisetti57@gmail.com>
2024-12-26 18:10:05 +0530

    python

---
+++ 5string concatenation.py
@@ -0,0 +1,16 @@
+a = "Kesava" + " " + "Bavisetti" 
+print (a)
+#adding of strings only 
+#it can't add integrs , float values
+
+#STRING REPETITION
+
+b = "Dhoni" * 7
+print (b)
+
+#using operator we can repeat n number of strings
+
+c = "cricket"
+c = ("* " * 3) + c + ("* " * 3)
+print (c)
+
