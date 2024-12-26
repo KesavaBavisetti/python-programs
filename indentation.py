566fcd7fbe2f68605167343bc59c474c99e6729f
Kesava Bavisetti <kesavabavisetti57@gmail.com>
2024-12-26 07:40:24 +0530

    python

---
+++ indentation.py
@@ -0,0 +1,6 @@
+if 7>3:
+    print ("seven is greater than three!")
+#identation means space before print statement 
+#hash symbol is used for comment line placed at end of a line
+#mainly its uesd for explains code or prevent python for executing
+"""we can write n number of multi line comments using triple quotes python will read code but it will igrone rest of line """"
