b7eb6a4c5879a15a826af42ab7dfc7821cec81d9
Kesava Bavisetti <kesavabavisetti57@gmail.com>
2024-12-26 08:03:20 +0530

    python

---
+++ variables.py
@@ -0,0 +1,9 @@
+x = 10
+#x is type of integer
+y = 5.7
+#y is type of float
+z = "kesava"
+#z is type string varibles can be declared either by using single or double quotes
+print (x)
+print (y)
+print (z)
