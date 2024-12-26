6e058b474f12cda7969ea315ff8725bca184ad52
Kesava Bavisetti <kesavabavisetti57@gmail.com>
2024-12-26 17:32:02 +0530

    python

---
+++ order of instrustions.py
@@ -0,0 +1,7 @@
+result = 7 + 5 * (2 ** 7) - 9 / 5
+print (result)
+#BODMAS rule stands for
+#B - Brackets first
+#O - Orders (exponents and square roots)
+#DM - Division and MUltipilication from left to right
+#AS - Addition and subtraction from left to right
