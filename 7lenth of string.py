4eb7a66487c9e17bc096c5588629622ed01127de
Kesava Bavisetti <kesavabavisetti57@gmail.com>
2024-12-27 09:52:41 +0530

    python

--- 7lenth of string.py
+++ 7lenth of string.py
@@ -5,3 +5,7 @@ length = len(name)
 print(length)
 
 #we can also find string lenght by using len() its built-in function 
+
+name = input("")
+greeting = input ("")
+print( greeting + "," + name "!")
---
+++ 8string last char.py
@@ -0,0 +1,7 @@
+str = input (" ")
+length = len(str)
+#len() built in function
+last_character = str[-1]
+#length of last char of sentence
+print("length of last str:", length)
+print("last character of the str:",last_character)
