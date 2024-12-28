8ad980dc63d1f52264f11981b5ff7643cd530ebb
Kesava Bavisetti <kesavabavisetti57@gmail.com>
2024-12-28 15:58:43 +0530

    python

---
+++ 10stringindexing.py
@@ -0,0 +1,18 @@
+#strchar means individual char of particular index
+#index first char = 0
+#last char = lenght -1
+
+str = "KESAVA"
+last_char =str [-1] #A
+second_last_char = str[-2] #V
+third_last_char = str[-3] #A
+fourth_last_char = str[-4] #S
+fiveth_last_char = str[-5] #E
+first_char = str[-6] #K
+
+print("last char:", last_char)
+print("second last char:", second_last_char)
+print("third last char:", third_last_char)
+print("fourth last char:", fourth_last_char)
+print("fiveth last char:", fiveth_last_char)
+print("first char:", first_char)
