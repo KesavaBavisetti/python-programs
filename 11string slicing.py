17c4154e7f9f7e5a95e668f4c7301c15cac3cde7
Kesava Bavisetti <kesavabavisetti57@gmail.com>
2024-12-28 16:40:07 +0530

    python

---
+++ 11string slicing.py
@@ -0,0 +1,13 @@
+#spliting the string of any char (extract) 
+str1 = "Mahesh Babu"
+substring = str1[0:6]
+#it will extract (7:11) babu and print (0:5) mahesh
+print(substring)
+
+#another example
+
+str2 = "Pawan Kalyan"
+#let split the first 5 char (pawan) 
+substring = str2[5:12]
+print(substring)
+#it will extract (0:4) pawan and print (5:12)kalyan
