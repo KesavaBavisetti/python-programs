440318a69b41aaef1d80013cb740f36ff8ecfae1
Kesava Bavisetti <kesavabavisetti57@gmail.com>
2024-12-31 09:50:51 +0530

    python

---
+++ 21relationaloperators.py
@@ -0,0 +1,30 @@
+#it means comparing two values
+#these are 6 types
+#1 equal to (==)
+#2 not equal to (!=)
+#3 greater than (>)
+#4 less than (<)
+#5 greater than or equal to (>=)
+#6 less than or equal to (<=)
+
+a = 7
+b = 2-9
+
+print (a == b)
+print (a != b)
+print (a > b)
+print (a < b)
+print (a >= b)
+print (a <= b)
+
+#another example
+
+a = 7
+b = 9-2
+
+print (a == b)
+print (a != b)
+print (a > b)
+print (a < b)
+print (a >= b)
+print (a <= b)
