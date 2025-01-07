3c887a2467315eafe5ae5e2b217666f1e673f084
Kesava Bavisetti <kesavabavisetti57@gmail.com>
2025-01-07 19:44:44 +0530

    python

---
+++ 26Forloop.py
@@ -0,0 +1,18 @@
+#for loop means code will execute in sequence (list , tuple , string or range)
+
+for num in range(1,6):
+#it will print 1 to 5 only (6 not included) 
+    print (num)
+
+#example for first 10 even numbers
+
+for num in range(2,21,2): 
+#it will print first 10 even numbers 2 to 20 with range 2
+#it start from 2 , increasing by 2 each upto 20 (21 excluded)
+    print (num)
+
+#example for first 10 odd numbers
+for num in range(1,20,2):
+#it will print first 10 odd numbers 1 to 19 with range 2
+#it starts from 1 , increasing by 2 upto 19 (20 excluded)
+    print(num)
