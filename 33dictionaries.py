c74ccb443f416819596df37163310d957a97045d
Kesava Bavisetti <kesavabavisetti57@gmail.com>
2025-01-23 19:35:38 +0530

    python

---
+++ 33dictionaries.py
@@ -0,0 +1,26 @@
+#it stores key-value pairs
+#we can modify (add / remove new data)
+#dictionaries items are represented in curly braces {} & each key-value seperated by colon :
+
+#creating dictionaries :
+#you can create a dictionary by enclosing key-value pairs in curly braces.
+# Example: Creating a dictionary of student information
+student = {
+    "name": "Kesava Baviestti",
+    "age": 21,
+    "major": "Electronics & communication",
+    "gpa": 8.7
+}
+
+#Accessing Values:
+#You can access values in a dictionary using keys. Dictionary keys are unique.
+# Example: Accessing values in a dictionary
+
+student = {
+    "name":"Kesava Baviestti",
+    "age": 21,
+    "major": "Electronics & communication",
+    "gpa": 8.7
+}
+print(student["name"])  
+print(student["gpa"])   
